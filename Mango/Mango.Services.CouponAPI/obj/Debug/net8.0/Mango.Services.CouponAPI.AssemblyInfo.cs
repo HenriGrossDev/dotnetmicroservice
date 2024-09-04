9b8d9d2af53d0cdcdd03b328c5babd630a8ba62b
HenriGrossDev <81596670+HenriGrossDev@users.noreply.github.com>
2024-09-04 04:26:27 -0300

    Add DbContext and ConnectionStrings

--- Mango/Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
+++ Mango/Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b60cbd1046dc1ac8b915d2678113c316bd4a8ff5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
