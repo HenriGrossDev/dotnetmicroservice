6c006f3211ff4a48d8e27f59bb3a620fb058438f
HenriGrossDev <81596670+HenriGrossDev@users.noreply.github.com>
2024-10-08 18:34:58 -0300

    Implements Common Response Dtro

--- Mango/Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
+++ Mango/Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b8d9d2af53d0cdcdd03b328c5babd630a8ba62b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd89e07c55fc2eff0b97fc16c9d40bb03ab254bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
