ef061eb79668ff21b29961374979565d2b7a9461
HenriGrossDev <81596670+HenriGrossDev@users.noreply.github.com>
2024-10-15 21:04:53 -0300

    Implement Register and Login endpoints

--- Mango/Mango.Services.AuthAPI/Models/Dto/UserDto.cs
+++ Mango/Mango.Services.AuthAPI/Models/Dto/UserDto.cs
@@ -2,9 +2,10 @@
 {
     public class UserDto
     {
-        public Guid Id { get; set; }
+        public Guid ID { get; set; }
         public string Email { get; set; }
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
+
     }
 }
